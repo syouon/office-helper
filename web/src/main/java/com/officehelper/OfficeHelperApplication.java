a044675419deb6c709e41750678dc8e673c05bc7
syouon <syou-on.do@mirakl.com>
2017-02-03 14:52:19 +0100

    Add spring boot dependencies and main class

---
+++ web/src/main/java/com/officehelper/OfficeHelperApplication.java
@@ -0,0 +1,12 @@
+package com.officehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficeHelperApplication.class, args);
+    }
+}
