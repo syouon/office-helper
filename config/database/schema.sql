cd9ed529d8a61c60820525a7d871566bf71bfc0f
syouon <syou-on.do@mirakl.com>
2017-02-08 11:57:54 +0100

    Add database init script

---
+++ config/database/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS officehelper_db;
+CREATE DATABASE officehelper_db;
+USE officehelper_db;
+
+CREATE TABLE user (
+  id    SERIAL,
+  email VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE request (
+  id              SERIAL,
+  title           VARCHAR(255) NOT NULL,
+  description     TEXT,
+  url             VARCHAR(255),
+  quantity        INT,
+  status          VARCHAR(10) NOT NULL,
+  creation_date   TIMESTAMP NOT NULL,
+  order_date      TIMESTAMP NULL,
+  reception_date  TIMESTAMP NULL,
+  user_id         BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE USER 'developer'@'localhost' IDENTIFIED BY 'd3v3l0p3r';
+GRANT ALL PRIVILEGES ON `officehelper_db`.* TO 'developer'@'localhost';
+FLUSH PRIVILEGES;
